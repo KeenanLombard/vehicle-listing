447537b44aabe9c5a96b2991473bf3c53c12ea6c
Keenan Lombard <keenanlombardd2@gmail.com>
2025-02-23 17:53:32 +0200

    filter by make using API not local storage

--- pages/index.vue
+++ pages/index.vue
@@ -1,88 +1,127 @@
 <!-- @format -->
-<!-- Author: Keenan Lombard -->
 
 <script setup>
-import { ref, computed } from "vue";
+import { ref, computed, watch } from "vue";
 import CarCard from "~/components/CarCard.vue";
 import Spinner from "~/components/Spinner.vue";
 
-const limit = 12; // limits items per page
+const limit = 12; // Limits items per page
 const currentPage = ref(1);
-const searchQuery = ref("");
+const selectedBrand = ref(""); // Default is 'All Brands'
 const sortOrder = ref("+make");
+const data = ref([]); // To store the fetched data
+const pending = ref(false); // To track loading state
+const error = ref(null); // To track error state
 
-//make request with page number, limit and sorting order
-const { data, pending, error, refresh } = useAsyncData("stock", async () => {
-  const response = await $fetch(
-    `https://crm.quickrentals.co.za/items/stock/?fields=*.*.*&limit=${limit}&page=${currentPage.value}&sort[]=${sortOrder.value}`
-  );
-  return response?.data || []; // Ensures it always returns an array
+// Function to fetch data
+const fetchCars = async () => {
+  pending.value = true;
+  error.value = null;
+
+  try {
+    let url = `https://crm.quickrentals.co.za/items/stock/?fields=*.*.*&limit=${limit}&page=${currentPage.value}&sort[]=${sortOrder.value}`;
+
+    if (selectedBrand.value) {
+      url += `&filter[_or][0][make][_contains]=${selectedBrand.value}`;
+    }
+
+    const response = await $fetch(url);
+    data.value = response?.data || [];
+  } catch (err) {
+    error.value = err;
+  } finally {
+    pending.value = false;
+  }
+};
+
+watch([selectedBrand, currentPage, sortOrder], () => {
+  if (selectedBrand.value) {
+    // Reset currentPage to 1 when selectedBrand changes
+    currentPage.value = 1;
+  }
+  fetchCars(); // Call fetchCars after the change
 });
+// Fetch data initially
+fetchCars();
 
-//Search local array with make, model and year
+// Computed property to filter cars based on search query
 const filteredCars = computed(() => {
-  if (!data.value) return []; // Return an empty array if data.value is null
-  if (!searchQuery.value) return data.value; // Return all cars if no search query
-  return data.value.filter((car) =>
-    `${car.make} ${car.model} ${car.year}`
-      .toLowerCase()
-      .includes(searchQuery.value.toLowerCase())
-  );
+  return data.value;
 });
 
 // Increment or Decrement currentPage
 const changePage = (page) => {
   currentPage.value = page;
-  refresh();
+};
+
+const resetFilters = () => {
+  selectedBrand.value = "";
+  sortOrder.value = "+make";
+  currentPage.value = 1;
 };
 
 // Function to change sort order
 const changeSort = (sort) => {
   sortOrder.value = sort;
-  refresh();
+  currentPage.value = 1;
 };
 </script>
 
 <template>
   <div class="container mx-auto p-6">
-    <!-- Search Bar -->
-    <div class="mb-6 flex">
-      <input
-        v-model="searchQuery"
-        placeholder="Search..."
-        class="w-full border border-gray-300 p-2 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
-        type="text" />
+    <!-- Filter by Brand Dropdown -->
 
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke-width="1.5"
-        stroke="currentColor"
-        class="size-10 my-auto text-blue-900 m-2 p-1">
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
-      </svg>
-    </div>
-
-    <!-- Sort By : Dropdown -->
-    <div class="mb-6 flex flex-wrap justify-end gap-4">
-      <div class="flex items-center space-x-2">
-        <label for="sort-cars" class="text-lg font-semibold">Sort by:</label>
+    <div
+      class="flex flex-col justify-between lg:flex-row md:flex-col sm:flex-col">
+      <div class="mb-6 flex flex-wrap justify-end gap-4">
+        <label for="brand-filter" class="text-lg my-auto font-semibold"
+          >Filter by Brand:</label
+        >
         <select
-          v-model="sortOrder"
-          id="sort-cars"
+          v-model="selectedBrand"
+          id="brand-filter"
           class="border border-gray-300 p-2 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
-          @change="changeSort(sortOrder)">
-          <option value="+make">Make (A-Z)</option>
-          <option value="-make">Make (Z-A)</option>
-          <option value="+year">Year (Oldest to Newest)</option>
-          <option value="-year">Year (Newest to Oldest)</option>
-          <option value="+monthly_rental">Price (Low to High)</option>
-          <option value="-monthly_rental">Price (High to Low)</option>
+          @change="fetchCars">
+          <option value="">All Brands</option>
+          <option value="Toyota">Toyota</option>
+          <option value="Ford">Ford</option>
+          <option value="Nissan">Nissan</option>
+          <option value="Hyundai">Hyundai</option>
+          <option value="Aprilia">Aprilia</option>
+          <option value="Audi">Audi</option>
+          <option value="Renault">Renault</option>
+          <option value="Maserati">Maserati</option>
+          <option value="Mercedes">Mercedes</option>
+          <option value="Kia">Kia</option>
+          <!-- Add more brands as needed -->
         </select>
+      </div>
+
+      <!-- Sort By : Dropdown -->
+      <div class="mb-6 flex flex-wrap justify-end gap-4">
+        <div class="flex items-center space-x-2">
+          <label for="sort-cars" class="text-lg font-semibold">Sort by:</label>
+          <select
+            v-model="sortOrder"
+            id="sort-cars"
+            class="border border-gray-300 p-2 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            @change="changeSort(sortOrder)">
+            <option value="+make">Make (A-Z)</option>
+            <option value="-make">Make (Z-A)</option>
+            <option value="+year">Year (Oldest to Newest)</option>
+            <option value="-year">Year (Newest to Oldest)</option>
+            <option value="+monthly_rental">Price (Low to High)</option>
+            <option value="-monthly_rental">Price (High to Low)</option>
+          </select>
+        </div>
+      </div>
+
+      <div class="mb-6 flex flex-wrap justify-end gap-4">
+        <button
+          @click="resetFilters()"
+          class="bg-blue-100 hover:bg-blue-200 text-blue-900 font-semibold rounded-xl p-4">
+          Reset All Filters
+        </button>
       </div>
     </div>
 
@@ -101,10 +140,9 @@ const changeSort = (sort) => {
       </p>
     </div>
 
-    <!-- Render Cards-->
+    <!-- Render Cars -->
     <div v-else class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
       <div
-        v-if="filteredCars && filteredCars.length !== 0"
         v-for="car in filteredCars"
         :key="car.id"
         class="bg-white cursor-pointer rounded-lg shadow-md hover:shadow-xl overflow-hidden transition-transform transform hover:scale-105 duration-300">
